76890b4fdc9cab38d55594803ab301c3f0ea04ac
DHAKE VAIBHAV NITIN <dhakev1@gmail.com>
2026-01-26 13:30:06 +0530

    fix

--- components/attributes/api.js
+++ components/attributes/api.js
@@ -1,110 +1,106 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+import { apiClient } from "@/services/apiClient";
 
-// Helper function for API calls
-const apiCall = async (endpoint, options = {}) => {
-  const url = `${API_BASE_URL}${endpoint}`;
-  
-  const defaultHeaders = {
-    'Content-Type': 'application/json',
-  };
-
-  const config = {
-    ...options,
-    headers: {
-      ...defaultHeaders,
-      ...options.headers,
-    },
-  };
-
-  try {
-    const response = await fetch(url, config);
-    const data = await response.json();
-    
-    if (!response.ok) {
-      throw new Error(data.message || 'Something went wrong');
-    }
-    
-    return data;
-  } catch (error) {
-    console.error('API Error:', error);
-    throw error;
-  }
-};
-
-// Attribute Set APIs
+/* =========================
+   ATTRIBUTE SET APIs
+========================= */
 export const attributeSetApi = {
-  getAll: (params = {}) => apiCall('/attributeSet', {
-    method: 'GET',
-    // headers: {
-    //   'pagination-query': JSON.stringify(params)
-    // }
-  }),
-  
-  getById: (id) => apiCall(`/attributeSet/${id}`),
-  
-  create: (data) => apiCall('/attributeSet', {
-    method: 'POST',
-    body: JSON.stringify(data),
-  }),
-  
-  update: (id, data) => apiCall(`/attributeSet/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-  }),
-  
-  delete: (id) => apiCall(`/attributeSet/${id}`, {
-    method: 'DELETE',
-  }),
+  getAll: (params = {}) =>
+    apiClient("/attributeSet", {
+      method: "GET",
+      // headers: {
+      //   "pagination-query": JSON.stringify(params),
+      // },
+    }),
+
+  getById: (id) =>
+    apiClient(`/attributeSet/${id}`, {
+      method: "GET",
+    }),
+
+  create: (data) =>
+    apiClient("/attributeSet", {
+      method: "POST",
+      body: data,
+    }),
+
+  update: (id, data) =>
+    apiClient(`/attributeSet/${id}`, {
+      method: "PUT",
+      body: data,
+    }),
+
+  delete: (id) =>
+    apiClient(`/attributeSet/${id}`, {
+      method: "DELETE",
+    }),
 };
 
-// Attribute APIs
+/* =========================
+   ATTRIBUTE APIs
+========================= */
 export const attributeApi = {
-  getAll: (params = {}) => apiCall('/attribute', {
-    method: 'GET',
-    // headers: {
-    //   'pagination-query': JSON.stringify(params)
-    // }
-  }),
-  
-  getById: (id) => apiCall(`/attribute/${id}`),
+  getAll: (params = {}) =>
+    apiClient("/attribute", {
+      method: "GET",
+      // headers: {
+      //   "pagination-query": JSON.stringify(params),
+      // },
+    }),
 
-  create: (data) => apiCall('/attribute', {
-    method: 'POST',
-    body: JSON.stringify(data),
-  }),
+  getById: (id) =>
+    apiClient(`/attribute/${id}`, {
+      method: "GET",
+    }),
 
-  update: (id, data) => apiCall(`/attribute/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-  }),
+  create: (data) =>
+    apiClient("/attribute", {
+      method: "POST",
+      body: data,
+    }),
 
-  delete: (id) => apiCall(`/attribute/${id}`, {
-    method: 'DELETE',
-  }),
+  update: (id, data) =>
+    apiClient(`/attribute/${id}`, {
+      method: "PUT",
+      body: data,
+    }),
+
+  delete: (id) =>
+    apiClient(`/attribute/${id}`, {
+      method: "DELETE",
+    }),
 };
 
-// Attribute Value APIs
+/* =========================
+   ATTRIBUTE VALUE APIs
+========================= */
 export const attributeValueApi = {
-  getAll: (params = {}) => apiCall('/attributeValue', {
-    method: 'GET',
-    // headers: {
-    //   'pagination-query': JSON.stringify(params)
-    // }
-  }),
-  
-  getById: (id) => apiCall(`/attributeValue/${id}`),
-  
-  create: (data) => apiCall('/attributeValue', {
-    method: 'POST',
-    body: JSON.stringify(data),
-  }),
-  
-  update: (id, data) => apiCall(`/attributeValue/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-  }),
-  
-  delete: (id) => apiCall(`/attributeValue/${id}`, {
-    method: 'DELETE',
-  }),
+  getAll: (params = {}) =>
+    apiClient("/attributeValue", {
+      method: "GET",
+      // headers: {
+      //   "pagination-query": JSON.stringify(params),
+      // },
+    }),
+
+  getById: (id) =>
+    apiClient(`/attributeValue/${id}`, {
+      method: "GET",
+    }),
+
+  create: (data) =>
+    apiClient("/attributeValue", {
+      method: "POST",
+      body: data,
+    }),
+
+  update: (id, data) =>
+    apiClient(`/attributeValue/${id}`, {
+      method: "PUT",
+      body: data,
+    }),
+
+  delete: (id) =>
+    apiClient(`/attributeValue/${id}`, {
+      method: "DELETE",
+    }),
 };
