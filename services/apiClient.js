558a2139d78e03f07d9ff7b7212adc731cdc5c88
DHAKE VAIBHAV NITIN <dhakev1@gmail.com>
2026-03-04 18:00:25 +0530

    fix

--- services/apiClient.js
+++ services/apiClient.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { store } from "@/redux/store";
 import { logout } from "@/redux/slices/authSlice";
 
-export const BASE_URL = "http://192.168.99.178:8000/api";
+// export const BASE_URL = "http://192.168.99.178:8000/api";
+
+
+export const BASE_URL = "http://localhost:8000/api";
 
 /* =========================
    🔑 JWT EXPIRY CHECK
